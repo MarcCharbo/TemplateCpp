0d00468b955ba74e5a15f1fec80954d9f65ea72f
Marc Charbonneau <mtl2bk@gmail.com>
2017-05-20 20:25:34 -0700

    adding gtest

---
+++ cmake-build-debug/CMakeFiles/TemplateAssignment.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TemplateAssignment.dir/main.cpp.o"
+  "TemplateAssignment.pdb"
+  "TemplateAssignment"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TemplateAssignment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
